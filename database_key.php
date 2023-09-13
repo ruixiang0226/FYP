99bf699d4650bf4922e49323a247a977834bff7f
ruixiang0226 <subscriberongruixiang@ipk.edu.my>
2023-09-13 15:53:36 +0800

    update

--- database_key.php
+++
@@ -1,7 +0,0 @@
-<?php
-// config.php
-define('DB_HOST', 'ilzyz0heng1bygi8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
-define('DB_USER', 'wdzd5d37qxl2zori');
-define('DB_PASSWORD', 'gnvgq0h5y6vmdhqr');
-define('DB_DATABASE', 'p40t91itwyub22ct');
-?>
