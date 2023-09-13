3abf924f8e1ced5957d403f5e5b4e54b78712811
ruixiang0226 <subscriberongruixiang@ipk.edu.my>
2023-09-13 14:59:53 +0800

    Update login.php

--- auth/controllers/login.php
+++ auth/controllers/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once(__DIR__ . '/database_key.php');
+require_once('/database_key.php');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 if ($conn->connect_error) {
