5de0aeef6c844b79fbc5254ac964d3a5be17eaa3
ruixiang0226 <subscriberongruixiang@ipk.edu.my>
2023-09-13 13:20:39 +0800

    Update login.php

--- auth/controllers/login.php
+++ auth/controllers/login.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-include '/config.php';
+include 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login_submit"])) {
     $email = $_POST["login_email"];
