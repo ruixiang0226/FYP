d399f91acdf57649f8de6e7ae378517759faa404
ruixiang0226 <subscriberongruixiang@ipk.edu.my>
2023-09-15 12:44:36 +0800

    update

--- api/process_admin.php
+++ api/process_admin.php
@@ -252,10 +252,10 @@ if ($stmt->execute()) {
     $vendorpage_id = $conn->insert_id;
     
     $filePaths = [
-        __DIR__ . '/../index.html',
-        __DIR__ . '/../user/user_account.php',
-        __DIR__ . '/../vendor_acc/vendor_account.php',
-        __DIR__ . '/../admin/admin.php'
+        'index.html',
+        'user/user_account.php',
+        'vendor_acc/vendor_account.php',
+        'admin/admin.php'
     ];   
 
     foreach ($filePaths as $homepageFilePath) {
